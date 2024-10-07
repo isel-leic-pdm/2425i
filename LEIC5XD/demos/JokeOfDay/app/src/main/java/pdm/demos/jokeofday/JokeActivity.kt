487bf58b62e59da7066f0db4a23b9ff2b7e25249
Paulo Pereira <palbp.giga@gmail.com>
2024-10-07 20:34:00 +0100

    Adds code written during the last lecture (JokeOfDay v.0.0.1)

--- LEIC5XD/demos/JokeOfDay/app/src/main/java/pdm/demos/jokeofday/JokeActivity.kt
+++ LEIC5XD/demos/JokeOfDay/app/src/main/java/pdm/demos/jokeofday/JokeActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import pdm.demos.jokeofday.domain.FakeJokesService
+import androidx.activity.viewModels
 
 const val TAG = "JOKE_OF_DAY"
 
@@ -29,11 +29,13 @@ const val TAG = "JOKE_OF_DAY"
  */
 class JokeActivity : ComponentActivity() {
 
+    private val viewModel by viewModels<JokeScreenViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JokeScreen(service = FakeJokesService())
+            JokeScreen(viewModel = viewModel)
         }
     }
 }
--- LEIC5XD/demos/JokeOfDay/app/src/main/java/pdm/demos/jokeofday/JokeScreen.kt
+++ LEIC5XD/demos/JokeOfDay/app/src/main/java/pdm/demos/jokeofday/JokeScreen.kt
@@ -1,55 +1,34 @@
 package pdm.demos.jokeofday
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
-import pdm.demos.jokeofday.domain.FakeJokesService
-import pdm.demos.jokeofday.domain.Joke
-import pdm.demos.jokeofday.domain.JokesService
 import pdm.demos.jokeofday.ui.common.ErrorAlert
 import pdm.demos.jokeofday.ui.theme.JokeOfDayTheme
-
-/**
- * Represents the state of the joke screen.
- */
-sealed interface JokeScreenState {
-    data object Idle : JokeScreenState
-    data object Loading : JokeScreenState
-    data class Success(val joke: Joke) : JokeScreenState
-    data class Error(val exception: Throwable) : JokeScreenState
-}
 
 /**
  * Root composable for the main screen, the one that displays a joke fetched from the used API.
  */
 @Composable
-fun JokeScreen(service: JokesService) {
+fun JokeScreen(viewModel: JokeScreenViewModel) {
     JokeOfDayTheme {
-        var state: JokeScreenState by remember { mutableStateOf(JokeScreenState.Idle) }
 
-        val currentState = state
+        val currentState = viewModel.state
         when (currentState) {
             is JokeScreenState.Idle -> IdleView(
-                onFetchRequested = {
-                    // TODO (1): Add implementation using coroutine scope and discuss its
-                    //  implications.
-                }
+                onFetchRequested = { viewModel.fetchJoke() }
             )
 
             is JokeScreenState.Loading -> LoadingView()
             is JokeScreenState.Success -> SuccessView(
                 joke = currentState.joke,
-                onFetchRequested = { state = JokeScreenState.Loading }
+                onFetchRequested = { viewModel.fetchJoke() }
             )
 
             is JokeScreenState.Error -> ErrorAlert(
                 title = R.string.error_api_title,
                 message = R.string.error_could_not_reach_api,
                 buttonText = R.string.error_retry_button_text,
-                onDismiss = { state = JokeScreenState.Loading }
+                onDismiss = { viewModel.fetchJoke() }
             )
         }
     }
@@ -58,5 +37,5 @@ fun JokeScreen(service: JokesService) {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun JokeScreenPreview() {
-    JokeScreen(service = FakeJokesService())
+    JokeScreen(viewModel = JokeScreenViewModel())
 }
